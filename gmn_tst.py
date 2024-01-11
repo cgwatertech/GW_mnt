f504d106e0d42eb420097a986e30bf6a59df7bbd
Clean Ground Water Tech Inc <cgwatertech@gmail.com>
2024-01-11 13:13:03 +0900

    Update gmn_tst.py

--- gmn_tst.py
+++ gmn_tst.py
@@ -69,4 +69,7 @@ st.markdown(f'<a href="data:file/csv;base64,{b64_all_data}" download="all_data.c
 
 # 선택 결과를 새로운 창에서 보여주기
 selected_data_preview = df[['Time', selected_location]]
+new_window = st.sidebar.empty()  # 새로운 창을 열기 위한 준비
+with new_window:
+
 st.write(selected_data_preview)
