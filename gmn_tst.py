a782815ce3ab28b3906cb2645c9a7c9f6a6f0bb2
Clean Ground Water Tech Inc <cgwatertech@gmail.com>
2024-02-29 13:59:20 +0900

    Update gmn_tst.py

--- gmn_tst.py
+++ gmn_tst.py
@@ -51,17 +51,22 @@ st.image("https://raw.githubusercontent.com/cgwatertech/GW_mnt/main/desKTOP_IMG.
 # Plot (오른쪽 아래 프레임)
 st.subheader(f"{selected_location} 위치의 지하수위 변화 ({start_datetime}부터 {end_datetime})")
 
+rng_cmn = 5
+
 # 선택한 위치에 대한 평균 값을 계산
 avg_value = filtered_data[selected_location].mean()
+mx_value = filtered_data[selected_location].max()
+mn_value = filtered_data[selected_location].min()
+rng_value = (mx_value - mn_value) * rng_cmn
+rng_vale = rng_value/2
 
 # 평균 값으로 새로운 데이터 프레임을 만듦
 avg_df = pd.DataFrame({'Time': filtered_data['Time'], selected_location: avg_value})
-
 # 그래프 그리기
 fig = px.line(filtered_data, x="Time", y=selected_location, title=f"{selected_location} 위치의 지하수위 변화 ({start_datetime}부터 {end_datetime})")
 
 # y 축 리미트 설정
-fig.update_layout(yaxis=dict(range=[avg_value - 17, avg_value + 17]))
+fig.update_layout(yaxis=dict(range=[avg_value - rng_vale, avg_value + rng_vale]))
 
 # x 축 tick 및 라벨 설정
 tickvals = filtered_data['Time'].iloc[::len(filtered_data) // 5]  # 5 ticks로 나누기
