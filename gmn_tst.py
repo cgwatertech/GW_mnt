fc99093c94d2f934ec1d07eff99aa29472d4f49f
Clean Ground Water Tech Inc <cgwatertech@gmail.com>
2024-03-04 19:04:54 +0900

    Update gmn_tst.py

--- gmn_tst.py
+++ gmn_tst.py
@@ -10,6 +10,8 @@ yxs = "GL"
 # Sidebar (왼쪽 프레임)
 st.sidebar.title("지하수위계")
 
+st.write(df.head())
+
 # 'Time'을 제외한 컬럼들을 선택 박스에 넣음
 selected_location = st.sidebar.selectbox("위치 선택", df.columns[1:])
 
